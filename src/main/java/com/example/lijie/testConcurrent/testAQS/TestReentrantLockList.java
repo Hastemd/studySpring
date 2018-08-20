5277c32e98188bbb484450c2fd1d11e6d23095af
李杰 <1246992412@qq.com>
2018-08-20 15:21:58 +0800

    java并发学习

---
+++ src/main/java/com/example/lijie/testConcurrent/testAQS/TestReentrantLockList.java
@@ -0,0 +1,10 @@
+package com.example.lijie.testConcurrent.testAQS;
+
+/**
+ * AQS的文档可以看为知笔记上的 并发编程高级篇3
+ * auther lijie  2018/8/12.
+ */
+public class TestReentrantLockList {
+
+
+}
---
+++ src/main/java/com/example/lijie/testConcurrent/testLockSupport/TestLockSupportThree.java
@@ -0,0 +1,31 @@
+package com.example.lijie.testConcurrent.testLockSupport;
+
+import java.util.concurrent.locks.LockSupport;
+
+import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
+
+/**
+ * auther lijie  2018/8/10.
+ */
+public class TestLockSupportThree {
+
+    public void testLockSupportThree(){
+        LockSupport.park(this);
+    }
+
+    public static void main(String[] args) {
+        //TestLockSupportThree testLockSupportThree = new TestLockSupportThree();
+        //testLockSupportThree.testLockSupportThree();
+
+        String tel = "18261613632";
+        String salt = "307";
+
+        String result = md5Hex(tel + salt);
+        System.out.println("md5加盐: " + result);
+
+        System.out.println("开始解密!");
+
+
+
+    }
+}
