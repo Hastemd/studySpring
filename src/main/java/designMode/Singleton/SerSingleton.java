094516833dd5547648ecb7a59e21b6ed4833129b
李杰 <1246992412@qq.com>
2019-05-06 14:23:42 +0800

    双重检查模式下创建的对象(private构造方法),是可以通过反射来创建新对象的

---
+++ src/main/java/designMode/Singleton/SerSingleton.java
@@ -0,0 +1,56 @@
+package designMode.Singleton;
+
+import java.io.*;
+
+/**
+ * auther lijie  2019/5/6.
+ */
+public class SerSingleton implements Serializable {
+
+    private volatile static SerSingleton uniqueInstance;
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private SerSingleton(){
+
+    }
+
+    public static SerSingleton getInstance(){
+        if(uniqueInstance == null){
+            synchronized (SerSingleton.class){
+                if(uniqueInstance == null){
+                    uniqueInstance = new SerSingleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        SerSingleton s = SerSingleton.getInstance();
+        s.setContent("单例序列化");
+        System.out.println("序列化前,读取其中的内容 : " + s.getContent());
+
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("SerSingleton.obj"));
+        oos.writeObject(s);
+        oos.flush();
+        oos.close();
+
+        FileInputStream fileInputStream = new FileInputStream("SerSingleton.obj");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        SerSingleton sRead = (SerSingleton)objectInputStream.readObject();
+        fileInputStream.close();
+
+        System.out.println(s);
+        System.out.println(sRead);
+        System.out.println("序列化后读取其中的内容："+sRead.getContent());
+        System.out.println("序列化前后两个是否同一个："+(s==sRead));
+    }
+}
